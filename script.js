6507c476112dec225d0d9887d48cf390a2a81aad
caroline cloughley <caro672g@stud.kea.dk>
2022-09-14 23:09:51 +0200

    data clean and displaying unstyled

--- script.js
+++ script.js
@@ -17,17 +17,32 @@ function loadJSON() {
 function prepareObjects(jsonData) {
   jsonData.forEach((studentInfo) => {
     let student = {
-      firstName: "harry",
-      lastName: "potter",
-      middleName: "the rock",
-      nickName: "specky",
-      image: "...",
-      house: "whatever",
+      firstName: "",
+      lastName: "",
+      middleName: "null",
+      nickName: "null",
+      image: "",
+      house: "",
     };
+    //takeaway the random spaces
     student.fullname = studentInfo.fullname.trim();
+    //get first name
     student.firstName = student.fullname.split(` `)[0];
-    student.lastName = student.fullname.substring(student.fullname.lastIndexOf(" "));
-    console.log(student.firstName);
-    console.log(student.lastName);
+    //capitalise firstName
+    student.firstName = capitalise(student.firstName);
+    //find lastName
+    student.lastName = student.fullname.substring(student.fullname.lastIndexOf(" ") + 1);
+    //capitalise lastName
+    student.lastName = capitalise(student.lastName);
+    //find nickName
+    if (student.fullname.includes(`"`)) {
+      student.nickName = student.fullname.split(`"`)[1];
+      student.nickName = capitalise(student.nickName);
+    }
+    console.log(student);
   });
+}
+
+function capitalise(str) {
+  return str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
 }
